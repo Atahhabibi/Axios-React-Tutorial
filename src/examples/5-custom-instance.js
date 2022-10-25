b8bb3441356e3737b8b7a559326c85bda347dafd
atahhabibi <habibiatah@gmail.com>
2022-10-25 12:08:52 -0700

    custom Instance for axios headers

--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,10 +1,18 @@
 import { useEffect } from 'react';
+import axios from 'axios';
+import authFetch from '../axios/Custom';
 
 const randomUserUrl = 'https://randomuser.me/api';
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+   try {
+    const resp1=await authFetch('/react-store-products')
+    const resp2=await axios(randomUserUrl)
+    
+   } catch (error) {
+    
+   }
   };
 
   useEffect(() => {
